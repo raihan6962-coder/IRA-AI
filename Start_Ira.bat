6ac616623e67329124a0369390af69c990ce1fc5
raihan6962-coder <raihan6962@gmail.com>
2026-07-25 22:48:41 +0600

    Update Start_Ira.bat

--- Start_Ira.bat
+++ Start_Ira.bat
@@ -2,7 +2,7 @@
 echo Starting Ira AI from GitHub...
 
 :: নিচে আপনার GitHub raw link বসান (কোটেশনের ভেতরে)
-set "RAW_LINK=YOUR_RAW_GITHUB_LINK_HERE"
+set "RAW_LINK=https://raw.githubusercontent.com/raihan6962-coder/IRA-AI/refs/heads/main/ira.py"
 
 :: cache-busting যাতে সবসময় সর্বশেষ কোড আসে
 curl -s -o ira.py "%RAW_LINK%?v=%random%"
